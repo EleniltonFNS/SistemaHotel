65e55cf6d0d5df48ebb8faee6b3962d5576a3a9f
Tiago Cardoso Ferreira <tiagocardoso1357@gmail.com>
2024-12-20 11:47:24 -0300

    recriação da classe Hospedes com seus atributos e métodos

--- Hotel/src/main/java/ReservasQuartos/Hospedes.java
+++ Hotel/src/main/java/ReservasQuartos/Hospedes.java
@@ -1,4 +1,57 @@
 package ReservasQuartos;
 
 public class Hospedes {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+
+    public Hospedes(String nome, String cpf, String telefone, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Hospedes{"
+                + "nome='" + nome + '\''
+                + ", cpf='" + cpf + '\''
+                + ", telefone='" + telefone + '\''
+                + ", email='" + email + '\''
+                + '}';
+    }
 }
