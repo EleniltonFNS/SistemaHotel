c22726a6cd1821be10eead674103097769f50822
Luanna-f <146787119+Luanna-f@users.noreply.github.com>
2024-12-20 15:18:37 -0300

    Classe TipoQuartos
    usados metodos construtor get e set e sobreposição

--- Hotel/src/main/java/ReservasQuartos/TipoQuartos.java
+++ Hotel/src/main/java/ReservasQuartos/TipoQuartos.java
@@ -1,4 +1,60 @@
 package ReservasQuartos;
 
 public class TipoQuartos {
+ private int id;
+    private String descricao;
+    private int capacidadePessoas;
+    private double precoDiaria;
+
+    // Constructor
+    public TipoQuartos(int id, String descricao, int capacidadePessoas, double precoDiaria) {
+        this.id = id;
+        this.descricao = descricao;
+        this.capacidadePessoas = capacidadePessoas;
+        this.precoDiaria = precoDiaria;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCapacidadePessoas() {
+        return capacidadePessoas;
+    }
+
+    public void setCapacidadePessoas(int capacidadePessoas) {
+        this.capacidadePessoas = capacidadePessoas;
+    }
+
+    public double getPrecoDiaria() {
+        return precoDiaria;
+    }
+
+    public void setPrecoDiaria(double precoDiaria) {
+        this.precoDiaria = precoDiaria;
+    }
+
+    @Override
+    public String toString() {
+        return "TipoQuartos{" +
+                "id=" + id +
+                ", descricao='" + descricao + '\'' +
+                ", capacidadePessoas=" + capacidadePessoas +
+                ", precoDiaria=" + precoDiaria +
+                '}';
+    }
 }
+
